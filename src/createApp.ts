f7b4626edd75bb74e16e45bf2db255e0b64fc962
wilsson <wilsonft83@gmail.com>
2020-01-08 22:19:08 -0500

    fix: add message warning and errors for validation name packagejson

--- src/createApp.ts
+++ src/createApp.ts
@@ -1,10 +1,19 @@
 import { resolve, basename, join } from 'path';
 import { copySync, ensureDirSync } from 'fs-extra';
 import { writeFileSync, readFileSync } from 'fs';
-import { spawn } from 'child_process';
+import { spawn, execSync } from 'child_process';
 import chalk from 'chalk';
 import { configTargets } from './configTargets';
 import { errorApp } from './errorApp';
+
+const hasYarn = (): boolean => {
+  try {
+    execSync('yarnpkg --version', { stdio: 'ignore' });
+    return true;
+  } catch(e) {
+    return false;
+  }
+};
 
 export const createApp = ({ name, target }): void => {
     if(!name) {
@@ -22,24 +31,25 @@ export const createApp = ({ name, target }): void => {
 }
 
 const installPackages = (name:string, dependency: string[]): void => {
-    let command: string = 'npm';
-    let args: string[] = [
-        'install',
-        '--save-dev'
-    ].concat(dependency);
-
+    let command = 'npm';
+    const useYarn = hasYarn();
+  
+    if(useYarn) {
+      command = 'yarn';
+    }
+  
     let config = { 
-        stdio: 'inherit',
-        shell: true
+      stdio: 'inherit',
+      shell: true
     };
 
     console.log('');
     console.log('Installing packages for your application');
-    const child = spawn(command, args, config);
+    const child = spawn(command, ['install'], config as any);
     child.on('close', () => {
         console.log('');
         console.log(`Project ${chalk.green(name)} created!`);
-        console.log(`use: cd ${chalk.green(name)} and ${chalk.green('npm start')}`);
+        console.log(`use: cd ${chalk.green(name)} and ${useYarn ? chalk.green('yarn dev'): chalk.green('npm run dev')}`);
         console.log('');
     });
 }
@@ -48,18 +58,25 @@ const validationAppName = (appName: string): void => {
     const validateProjectName = require( 'validate-npm-package-name');
     let results = validateProjectName(appName);
     let dependency = ['webpack', 'webpack-dev-server'];
+
     if(!results.validForNewPackages){
-        console.error(`Could not create project named: ${chalk.red(appName)}`);
-        console.log('please correct:');
-        results.errors.forEach(error => {
-            console.log(`    ${chalk.red('*')} ${error}`);
-        });
-        process.exit(1);
+      console.error(`Could not create project named: ${chalk.red(appName)}`);
+      console.log('please correct:');
+  
+      results.errors && results.errors.forEach(error => {
+        console.log(`    ${chalk.red('*')} ${error}`);
+      });
+  
+      results.warnings && results.warnings.forEach(error => {
+        console.log(`    ${chalk.red('*')} ${error}`);
+      });
+      process.exit();
     }
+
     if(dependency.indexOf(appName) !== -1){
         console.error(`Could not create project named ${chalk.red(appName)}.`);
         console.error('Please change the name of the application, a dependency has the same name.');
-        process.exit(1);
+        process.exit();
     }
 }
 
